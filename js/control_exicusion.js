240f91476c7d0508c49df3e0ae19038ff80095be
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-02-03 21:58:22 +0600

    switch statemetn complete

--- js/control_exicusion.js
+++ js/control_exicusion.js
@@ -15,4 +15,19 @@
 	}
 
 	(1) ? console.log("This is if function") : console.log("This is else function0");
+
+// switch statement
+var switcher = "Sabto"
+switch (switcher) {
+	case "Panto":
+		console.log("This is Pranto");
+		break;
+	case "Sabto":
+		console.log("This is Santo");
+		break;
+	default:
+		console.log("This is not Santo and Pranto");
+		break;
+}
+
 }());
