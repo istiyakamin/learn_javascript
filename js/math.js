50d9b0e2f023ca83a7647aca8cb64f693d9d20a5
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-02-04 23:49:05 +0600

    working with math element

---
+++ js/math.js
@@ -0,0 +1,21 @@
+(function () {
+	"use strict";
+	console.log(Math.PI);
+	console.log(Math.ceil(1.8));
+	console.log(Math.floor(1.8));
+
+	console.log(Math.round(4.4));
+	console.log(Math.round(4.6));
+
+	console.log(Math.max(1,2,3,4,5,6,7,8,9));
+	console.log(Math.min(1,2,3,4,5,6,7,8,9));
+
+	console.log(Math.abs(-10));
+	console.log(Math.sqrt(256));
+
+	console.log(Math.pow(5, 2));
+	console.log(Math.random());
+
+	console.log(Math.ceil(Math.random() * 10));
+
+}())
