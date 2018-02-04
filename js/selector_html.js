0a7b394187f48c8face61c85ee9c9a1d4e26f15f
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-02-05 00:21:18 +0600

    html selector

---
+++ js/selector_html.js
@@ -0,0 +1,8 @@
+(function () {
+	"use strict";
+
+	console.log(document.getElementById('header'));
+	console.log(document.getElementsByClassName('title'));
+
+	console.log(document.getElementsByTagName('p'));
+}())
