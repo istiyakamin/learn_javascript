5684b48852e2b677077e897efc5dd681a40ab586
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-02-05 22:17:45 +0600

    add outer or inner html

--- js/selector_html.js
+++ js/selector_html.js
@@ -5,4 +5,17 @@
 	console.log(document.getElementsByClassName('title'));
 
 	console.log(document.getElementsByTagName('p'));
+
+	var fullID = document.getElementById('header');
+	fullID.id = "changeId";
+	fullID.classList.add('also_another');
+	fullID.classList.remove('also_another');
+	console.log(fullID.className);
+	console.log(fullID.classList.contains('also'));
+
+	console.log(fullID.tagName);
+
+	fullID.innerHTML = '<div>' + fullID.innerHTML + '</div>';
+	fullID.outerHTML = '<section>' + fullID.outerHTML + '</section>';
+
 }())
