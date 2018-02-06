0ed252f4c13ec9b7c9beb22c085251ab69dcbcd0
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-02-06 23:12:23 +0600

    session storage complete

---
+++ js/storage.js
@@ -0,0 +1,25 @@
+(function () {
+	"use strict";
+
+	console.log(window.sessionStorage);
+
+	window.sessionStorage.setItem('item1', true);
+	window.sessionStorage.setItem('item2', "hellow world");
+	window.sessionStorage.setItem('item3', ['one', 'two']);
+	//window.sessionStorage.setItem('item4', JSON.stringify({first_name:'istiyak', last_name:'amin'}));
+
+	console.log(JSON.parse(window.sessionStorage.getItem('item4')));
+
+	function readAll() {
+		var x, length;
+		for(x=0, length = window.sessionStorage.length; x < length; x++){
+			console.log(window.sessionStorage.key(x));
+		}
+	}
+	
+
+	window.sessionStorage.removeItem('item1');
+	window.sessionStorage.clear();
+	readAll()
+
+}());
